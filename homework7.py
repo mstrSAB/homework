0c3e72e2db60f432ad8cf3f7a6c41578c05ac9bc
mstrSAB <163666695+mstrSAB@users.noreply.github.com>
2024-04-06 22:56:01 +0700

    homework7

---
+++ homework7.py
@@ -0,0 +1,12 @@
+auto_list = ['BMW', 'MB', 'LADA', 'KIA', 'HONDA']
+#print('Я езжу на ',(auto_list[2]))
+for i in auto_list:
+        print('Я езжу на автомобиле марки',i)
+
+cars_count = 0
+for i in range(len(auto_list)):
+        cars_count += 10
+print(cars_count)
+
+
+
